937f1e208978c9d702a5b6387763c8b8d10c0d07
Tim Guggenmos <tim.guggenmos@me.com>
2020-02-12 07:21:08 +0100

    renamed to _Template to enable macOS dark-light mode switching

--- bin/generate-images.sh
+++ bin/generate-images.sh
@@ -1,4 +1,4 @@
 # npm install svgexport -g
 svgexport svg/icon.svg images/icon.png
-svgexport svg/symbol.svg images/symbol.png 16:16
-svgexport svg/symbol.svg images/symbol@2x.png 32:32
+svgexport svg/symbol.svg images/symbolTemplate.png 16:16
+svgexport svg/symbol.svg images/symbolTemplate@2x.png 32:32
