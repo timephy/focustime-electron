0a6735ef5472b3eeca7a4c2b00ed93ab7781f7e1
Tim Guggenmos <tim.guggenmos@me.com>
2020-02-12 07:23:23 +0100

    update

--- bin/generate-images.sh
+++ bin/generate-images.sh
@@ -1,4 +1,6 @@
 # npm install svgexport -g
+mkdir -p images
+
 svgexport svg/icon.svg images/icon.png
 svgexport svg/symbol.svg images/symbolTemplate.png 16:16
 svgexport svg/symbol.svg images/symbolTemplate@2x.png 32:32
